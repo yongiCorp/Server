308dff92e940fb62a07615e77adfbe3fc8376205
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-01-06 15:45:29 +0900

    초기 설정

---
+++ src/main/java/com/brandol/BrandolApplication.java
@@ -0,0 +1,13 @@
+package com.brandol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandol/BrandolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
