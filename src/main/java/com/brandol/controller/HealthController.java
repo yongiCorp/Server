fcad2e2e402dd1854bb9cbcef4cc56720fbc2a01
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-01-21 18:03:03 +0900

    Feat/5 ci/cd health 체크용 API

---
+++ src/main/java/com/brandol/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.brandol.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+    @GetMapping("/health")
+    public String healthAPI(){
+        return "I'm Healthy";
+    }
+}
