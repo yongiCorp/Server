ed8b3e93713bc20eb90d29c99688d6ed6b3ea9ce
Lee Jong In <leeji88460@gmail.com>
2024-01-26 01:39:20 +0900

    feat/6 주석 정리

--- src/main/java/com/brandol/apiPayload/exception/ErrorHandler.java
+++ src/main/java/com/brandol/apiPayload/exception/ErrorHandler.java
@@ -2,7 +2,7 @@ package com.brandol.apiPayload.exception;
 
 import com.brandol.apiPayload.code.BaseCode;
 
-public class ErrorHandler extends GeneralException{
+public class ErrorHandler extends GeneralException{ //예외 처리 전담 핸들러
     public ErrorHandler(BaseCode baseCode){
         super(baseCode);
     }
