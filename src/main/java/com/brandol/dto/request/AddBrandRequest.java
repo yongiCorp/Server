38c3ce8ea9b227b82abcf1b79c79ac419b5283f8
Lee Jong In <leeji88460@gmail.com>
2024-01-27 22:50:32 +0900

    feat/12 스웨거 작성 완료

--- src/main/java/com/brandol/dto/request/AddBrandRequest.java
+++ src/main/java/com/brandol/dto/request/AddBrandRequest.java
@@ -1,6 +1,7 @@
 package com.brandol.dto.request;
 
 import com.brandol.domain.Brand;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,13 +15,16 @@ import javax.validation.constraints.NotNull;
 @Setter
 @Valid
 public class AddBrandRequest{ // 브랜드 신규등록 DTO
-
+    @Schema(description = "등록할 브랜드의 이름")
     @NotNull
     private String name;
+    @Schema(description = "등록할 브랜드의 소개")
     @NotNull
     private String description;
+    @Schema(description = "등록할 브랜드의 프로필 이미지")
     @NotNull
     private MultipartFile profileImage;
+    @Schema(description = "등록할 브랜드의 배경 이미지")
     @NotNull
     private MultipartFile backgroundImage;
 
