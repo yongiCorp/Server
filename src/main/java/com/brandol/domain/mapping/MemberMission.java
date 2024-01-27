54a6fd4b89195898403f75b725e4434c2eca3c87
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-01-27 17:30:19 +0900

    Feat/10 미션 엔티티 생성

---
+++ src/main/java/com/brandol/domain/mapping/MemberMission.java
@@ -0,0 +1,31 @@
+package com.brandol.domain.mapping;
+
+import com.brandol.domain.Member;
+import com.brandol.domain.Mission;
+import com.brandol.domain.enums.MissionStatus;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="member_mission_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id", nullable = false)
+    private Mission mission;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus missionStatus;
+}
