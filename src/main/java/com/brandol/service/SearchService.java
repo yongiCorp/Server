bf51ff4ec6119508798e38965d5dc25e8ef79fb4
bigwaveBigwave <bluebiird909@naver.com>
2024-01-29 21:52:13 +0900

    FEAT/11 검색 메인 페이지 - 컨텐츠 조회 시 작성자 이름, 작성자 프로필 사진, 브랜드 이름 추가

--- src/main/java/com/brandol/service/SearchService.java
+++ src/main/java/com/brandol/service/SearchService.java
@@ -4,11 +4,11 @@ import com.brandol.domain.Brand;
 import com.brandol.domain.Contents;
 import com.brandol.domain.Items;
 import com.brandol.domain.Member;
-import com.brandol.dto.response.SearchResponse;
-import com.brandol.dto.subDto.SearchAvatarstoreList;
-import com.brandol.dto.subDto.SearchBrandList;
-import com.brandol.dto.subDto.SearchContentsList;
-import com.brandol.dto.subDto.SearchUserList;
+import com.brandol.dto.response.SearchMainResponse;
+import com.brandol.dto.subDto.SearchMainAvatarstoreList;
+import com.brandol.dto.subDto.SearchMainBrandList;
+import com.brandol.dto.subDto.SearchMainContentsList;
+import com.brandol.dto.subDto.SearchMainUserList;
 import com.brandol.repository.SearchAvatarstoreRepository;
 import com.brandol.repository.SearchBrandRepository;
 import com.brandol.repository.SearchContentsRepository;
@@ -35,27 +35,33 @@ public class SearchService {
 
 
     @Transactional
-    public SearchResponse Makesearchpage(){
+    public SearchMainResponse makeSearchpage(){
 
 
 
         // 브랜드 리스트
         List<Brand> searchbrandList = sbr.findThreeByRandom();
-        Map<String,Object> brandList = SearchBrandList.createsearchBrandList(searchbrandList);
+        Map<String,Object> brandList = SearchMainBrandList.createsearchBrandList(searchbrandList);
 
         // 멤버 리스트
         List<Member> searchuserList = sur.findThreeByRandom();
-        Map<String,Object> memberList = SearchUserList.createsearchuserList(searchuserList);
+        Map<String,Object> memberList = SearchMainUserList.createsearchuserList(searchuserList);
 
         // 컨텐츠 리스트
         Map<String,Object> contentList = new LinkedHashMap<>();
         List<Contents> targetContentsList = scr.findAll();
+        List<Member> targetMemberList = sur.findAll();
+        List<Brand> targetBrandList = sbr.findAll();
         for(int i = 0; i < 3; i++) {
             contentList.put("searchcontents" + i
-                    , SearchContentsList.createsearchcontentsList(targetContentsList.get(i)
+                    , SearchMainContentsList.createsearchcontentsList(targetContentsList.get(i)
                     , scr.countLikesByContents_id(targetContentsList.get(i).getId())
                     , scr.countCommentsByContents_id(targetContentsList.get(i).getId())
+                    ,targetBrandList.get(i)
+                    ,targetMemberList.get(i)
                     ));
+
+
 
         }
 
@@ -63,9 +69,9 @@ public class SearchService {
 
         // 아바타스토어 리스트
         List<Items> searchavatarstoreList = sar.findThreeByRandom();
-        Map<String,Object> avatarstoreList = SearchAvatarstoreList.createsearchavatarstorelist(searchavatarstoreList);
+        Map<String,Object> avatarstoreList = SearchMainAvatarstoreList.createsearchavatarstorelist(searchavatarstoreList);
 
-        return SearchResponse.MainPage(brandList,memberList,contentList,avatarstoreList);
+        return SearchMainResponse.mainPage(brandList,memberList,contentList,avatarstoreList);
 
 
 
