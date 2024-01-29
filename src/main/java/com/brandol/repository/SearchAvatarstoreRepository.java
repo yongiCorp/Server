6f27f5b9699bef8a2bf15bc152ce5814d332bd2e
bigwaveBigwave <bluebiird909@naver.com>
2024-01-29 14:26:20 +0900

    FEAT/11 유저, 아바타 스토어 랜덤 조회 추가

--- src/main/java/com/brandol/repository/SearchAvatarstoreRepository.java
+++ src/main/java/com/brandol/repository/SearchAvatarstoreRepository.java
@@ -1,8 +1,13 @@
 package com.brandol.repository;
 
-import com.brandol.domain.Brand;
+
 import com.brandol.domain.Items;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SearchAvatarstoreRepository extends JpaRepository<Items, Long> {
+    @Query(value = "SELECT * FROM Items  ORDER BY RAND() limit 3",nativeQuery = true)
+    List<Items> findThreeByRandom();
 }
--- src/main/java/com/brandol/repository/SearchUserRepository.java
+++ src/main/java/com/brandol/repository/SearchUserRepository.java
@@ -1,8 +1,13 @@
 package com.brandol.repository;
 
-import com.brandol.domain.Brand;
+
 import com.brandol.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SearchUserRepository extends JpaRepository<Member, Long> {
+    @Query(value = "SELECT * FROM Member  ORDER BY RAND() limit 3",nativeQuery = true)
+    List<Member> findThreeByRandom();
 }
