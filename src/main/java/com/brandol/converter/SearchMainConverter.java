2b6939038f11a72fc1006256b82ef4ea1db8a3d1
bigwaveBigwave <bluebiird909@naver.com>
2024-02-01 11:02:31 +0900

    FEAT/11 검색 메인 페이지 조회 Dto 리팩토링 및 컨버터 추가

---
+++ src/main/java/com/brandol/converter/SearchMainConverter.java
@@ -0,0 +1,72 @@
+package com.brandol.converter;
+
+import com.brandol.domain.Brand;
+import com.brandol.domain.Contents;
+import com.brandol.domain.Items;
+import com.brandol.domain.Member;
+import com.brandol.domain.enums.MemberListStatus;
+import com.brandol.domain.mapping.MemberBrandList;
+import com.brandol.dto.response.BrandResponseDto;
+import com.brandol.dto.response.SearchMainResponseDto;
+
+import java.util.List;
+
+public class SearchMainConverter {
+    public static SearchMainResponseDto.SearchMainBrandDto toSearchMainBrandDto(Brand brand) {
+        return SearchMainResponseDto.SearchMainBrandDto.builder()
+                .brandId(brand.getId())
+                .brandName(brand.getName())
+                .brandDescription(brand.getDescription())
+                .brandProfileImage(brand.getProfileImage())
+                .build();
+    }
+    public static SearchMainResponseDto.SearchMainUserDto toSearchMainUserDto(Member member) {
+        return SearchMainResponseDto.SearchMainUserDto.builder()
+                .userId(member.getId())
+                .userName(member.getName())
+                .userAvatar(member.getAvatar())
+                .build();
+    }
+    public static SearchMainResponseDto.SearchMainContentsDto toSearchMainContentsDto(
+            Contents contents,
+            List<String> contentsImages
+            ) {
+        return SearchMainResponseDto.SearchMainContentsDto.builder()
+                .contentsId(contents.getId())
+                .contentsTitle(contents.getTitle())
+                .content(contents.getContent())
+                .images(contentsImages)
+                .likeCount(contents.getLikes())
+                .commentCount(contents.getComments())
+                .writerId(contents.getMember().getId())
+                .writerName(contents.getMember().getName())
+                .writerProfile(contents.getMember().getAvatar())
+                .createdDate(contents.getCreatedAt())
+                .build();
+    }
+    public static SearchMainResponseDto.SearchMainAvatarStoreDto toSearchMainAvatarStoreDto(Items items) {
+        return SearchMainResponseDto.SearchMainAvatarStoreDto.builder()
+                .itemId(items.getId())
+                .itemsName(items.getName())
+                .itemDescription(items.getDescription())
+                .brandName(items.getBrand().getName())
+                .itemImage(items.getImage())
+                .itemPart(String.valueOf(items.getItemPart()))
+                .itemPrice(items.getPrice())
+                .build();
+    }
+
+    public static SearchMainResponseDto.SearchMainAllDto tosearchMainAllDto(
+            List<SearchMainResponseDto.SearchMainBrandDto> searchMainBrandDtoList,
+            List<SearchMainResponseDto.SearchMainUserDto> searchMainUserDtoList,
+            List<SearchMainResponseDto.SearchMainContentsDto> searchMainContentsDtoList,
+            List<SearchMainResponseDto.SearchMainAvatarStoreDto> searchMainAvatarStoreDtoList){
+        return SearchMainResponseDto.SearchMainAllDto.builder()
+                .searchMainBrandDto(searchMainBrandDtoList)
+                .searchMainUserDto(searchMainUserDtoList)
+                .searchMainContentsDto(searchMainContentsDtoList)
+                .searchMainAvatarStoreDto(searchMainAvatarStoreDtoList)
+                .build();
+    }
+
+}
--- src/main/java/com/brandol/dto/response/SearchMainResponseDto.java
+++ src/main/java/com/brandol/dto/response/SearchMainResponseDto.java
@@ -1,36 +1,79 @@
 package com.brandol.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class SearchMainResponseDto {
-    private Map<String,Object> searchbrandList = new HashMap<>();
-    private Map<String,Object> searchuserList = new HashMap<>();
-    private Map<String,Object> searchcontentsList = new HashMap<>();
-    private Map<String,Object> searchavatarstorelist = new HashMap<>();//얘는 보이는데 안의 브랜드 네임 등이 안보임
 
 
-    public static SearchMainResponseDto mainPage(Map<String,Object> searchbrandList,
-                                                 Map<String,Object> searchuserList,
-                                                 Map<String,Object> searchcontents,
-                                                 Map<String,Object> searchavatarstores
-                                          ){
-        return  SearchMainResponseDto.builder()
-                .searchbrandList(searchbrandList)
-                .searchuserList(searchuserList)
-                .searchcontentsList(searchcontents)
-                .searchavatarstorelist(searchavatarstores)
-                .build();
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchMainAllDto {
+        private List<SearchMainBrandDto> searchMainBrandDto;
+        private List<SearchMainUserDto> searchMainUserDto;
+        private List<SearchMainContentsDto> searchMainContentsDto;
+        private List<SearchMainAvatarStoreDto> searchMainAvatarStoreDto;
+    }
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchMainBrandDto {
+        private Long brandId;
+        private String brandName;
+        private String brandProfileImage;
+        private String brandDescription;
 
     }
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchMainUserDto {
+        private Long userId;
+        private String userName;
+        private String userAvatar;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchMainContentsDto {
+        private Long contentsId;
+        private String contentsTitle;
+        private String content;
+        private List<String> images;
+        private int likeCount;
+        private int commentCount;
+        private Long writerId;
+        private String writerName;
+        private String writerProfile;
+        private LocalDateTime createdDate;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchMainAvatarStoreDto {
+        private Long itemId;
+        private String itemsName;
+        private String itemPart;
+        private String brandName;
+        private String itemImage;
+        private String itemDescription;
+        private int itemPrice;
+
+    }
+
 }
