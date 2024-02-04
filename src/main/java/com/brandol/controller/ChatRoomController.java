837e2c73cb5435e3c9bb18d22bae303023823540
bigwaveBigwave <bluebiird909@naver.com>
2024-02-04 15:42:23 +0900

    FEAT/40 컨트롤러 생성

---
+++ src/main/java/com/brandol/controller/ChatRoomController.java
@@ -0,0 +1,20 @@
+package com.brandol.controller;
+
+import com.brandol.apiPayload.ApiResponse;
+import com.brandol.apiPayload.code.status.SuccessStatus;
+import com.brandol.dto.response.SearchResponseDto;
+import com.brandol.service.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomController {
+
+
+}
