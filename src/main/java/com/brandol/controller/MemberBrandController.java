69d3da0f715846bd87ea012a13d3964a67ce69f8
Lee Jong In <leeji88460@gmail.com>
2024-02-08 11:43:43 +0900

    memberMission 오류나는 부분만 주석처리

--- src/main/java/com/brandol/controller/MemberBrandController.java
+++ src/main/java/com/brandol/controller/MemberBrandController.java
@@ -35,7 +35,7 @@ public class MemberBrandController {
     public ApiResponse<String> addMemberBrandList(@PathVariable("brandId")Long brandId, Authentication authentication){
         Long memberId = Long.parseLong(authentication.getName());
         memberService.addMemberBrandList(memberId, brandId);
-        memberMissionService.checkAddMission(memberId);
+        //memberMissionService.checkAddMission(memberId);
         return ApiResponse.onSuccess(SuccessStatus._CREATED.getCode(), SuccessStatus._CREATED.getMessage(), null);
     }
 
