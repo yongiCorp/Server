ceb180aa057bb934e48984e9bafb9683ade84671
bigwaveBigwave <bluebiird909@naver.com>
2024-02-10 00:16:15 +0900

    Feat/91 채팅 라이브러리 추가, WebsocketBrokerConfig 파일 설정

---
+++ src/main/java/com/brandol/config/WebSocketBrokerConfig.java
@@ -0,0 +1,27 @@
+package com.brandol.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOriginPatterns("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        registry.enableSimpleBroker("/sub");
+
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
