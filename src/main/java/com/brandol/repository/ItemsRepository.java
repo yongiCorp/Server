cae2f354a1724e0ed0616dc5be4b73934e1c2be1
seungheon123 <seungheon.bisb.2012@gmail.com>
2024-02-15 22:49:29 +0900

    Refactor/117 native query 수정

--- src/main/java/com/brandol/repository/ItemsRepository.java
+++ src/main/java/com/brandol/repository/ItemsRepository.java
@@ -9,16 +9,16 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ItemsRepository extends JpaRepository<Items, Long> {
-    @Query(value = "SELECT * FROM Items  ORDER BY RAND() limit 3",nativeQuery = true)
+    @Query(value = "SELECT * FROM items  ORDER BY RAND() limit 3",nativeQuery = true)
     List<Items> findThreeByRandom();
 
-    @Query(value = "SELECT * FROM Items  ORDER BY RAND()",nativeQuery = true)
+    @Query(value = "SELECT * FROM items  ORDER BY RAND()",nativeQuery = true)
     List<Items> findTotalByRandom();
 
-    @Query(value = "SELECT * FROM Items i WHERE i.item_part like :itemPart ORDER BY RAND() ",nativeQuery = true)
+    @Query(value = "SELECT * FROM items i WHERE i.item_part like :itemPart ORDER BY RAND() ",nativeQuery = true)
     List<Items> finditemPartByRandom(@Param("itemPart") String itemPart);
 
-    @Query(value = "SELECT * FROM Items i  ORDER BY CASE WHEN i.items_id = :itemId THEN 0 ELSE 1 END, RAND()",nativeQuery = true)
+    @Query(value = "SELECT * FROM items i  ORDER BY CASE WHEN i.items_id = :itemId THEN 0 ELSE 1 END, RAND()",nativeQuery = true)
     List<Items> finditemByIdandRandom(@Param("itemId") Long itemId);
 
 
