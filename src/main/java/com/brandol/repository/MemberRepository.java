ba93b3dd23ff3e24f022b22261181fee013d7dc3
ziy00n <jiyun4188@gmail.com>
2024-02-19 00:48:14 +0900

    ♻️ [Refactor] 브랜드 관리자는 유저 조회 안되도록 수정

--- src/main/java/com/brandol/repository/MemberRepository.java
+++ src/main/java/com/brandol/repository/MemberRepository.java
@@ -12,10 +12,10 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Query(value = "select m from Member m WHERE m.id = :id")
     Member findOneById(@Param("id") Long id);
 
-    @Query(value = "SELECT * FROM member ORDER BY RAND() limit 3",nativeQuery = true)
+    @Query(value = "SELECT * FROM member WHERE member.role = 'ROLE_USER' ORDER BY RAND() LIMIT 3", nativeQuery = true)
     List<Member> findThreeByRandom();
 
-    @Query(value = "SELECT * FROM member ORDER BY RAND() ",nativeQuery = true)
+    @Query(value = "SELECT * FROM member  WHERE member.role = 'ROLE_USER' ORDER BY RAND() ",nativeQuery = true)
     List<Member> findAllByRandom();
 
     Optional<Member> findByEmail(String email);
